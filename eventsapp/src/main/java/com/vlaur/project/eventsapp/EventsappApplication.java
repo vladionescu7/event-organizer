45ab6313f3144096c4bc638e7a6b412a879e5030
Vlad Ionescu <vlad.ionescu@yahoo.com>
2020-02-02 10:05:20 +0200

    - first commit

---
+++ eventsapp/src/main/java/com/vlaur/project/eventsapp/EventsappApplication.java
@@ -0,0 +1,13 @@
+package com.vlaur.project.eventsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsappApplication.class, args);
+	}
+
+}
---
+++ eventsapp/src/test/java/com/vlaur/project/eventsapp/EventsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlaur.project.eventsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
