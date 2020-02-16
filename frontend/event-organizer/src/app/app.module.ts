2e2d6b87eb6435dd2e095fab1c1ee4dbf1692ebf
laurmihai88 <panaete.laurentiu@gmail.com>
2020-02-16 16:09:29 +0200

    created Comment dto

--- frontend/event-organizer/src/app/app.module.ts
+++ frontend/event-organizer/src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { FeaturesModule } from './features/features.module';
+import { IntroComponent } from './intro/intro.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './features/user/user.component';
+import { MaterialModule } from './shared/material/material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FeaturesModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
