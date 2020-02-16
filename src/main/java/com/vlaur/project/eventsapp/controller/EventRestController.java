a709de6bfec5ab2bd52515c5977575106252f78f
Vlad Ionescu <vlad.ionescu@yahoo.com>
2020-02-16 12:54:41 +0200

    -added create method in EventRestController -tests work

--- src/main/java/com/vlaur/project/eventsapp/controller/EventRestController.java
+++ src/main/java/com/vlaur/project/eventsapp/controller/EventRestController.java
@@ -1,5 +1,19 @@
 package com.vlaur.project.eventsapp.controller;
 
+import com.vlaur.project.eventsapp.dto.comment.CommentMapper;
+import com.vlaur.project.eventsapp.dto.event.EventMapper;
+import com.vlaur.project.eventsapp.dto.event.EventRequest;
+import com.vlaur.project.eventsapp.dto.event.EventResponse;
+import com.vlaur.project.eventsapp.dto.user.UserMapper;
+import com.vlaur.project.eventsapp.model.Event;
+import com.vlaur.project.eventsapp.service.CommentService;
+import com.vlaur.project.eventsapp.service.EventService;
+import com.vlaur.project.eventsapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +22,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventRestController {
 
     public static final String API_EVENTS = "/api/events";
+
+    private EventService eventService;
+    private EventMapper eventMapper;
+    private CommentService commentService;
+    private CommentMapper commentMapper;
+    private UserService userService;
+    private UserMapper userMapper;
+
+    @Autowired
+    public EventRestController(EventService eventService, EventMapper eventMapper, CommentService commentService,
+                               CommentMapper commentMapper, UserService userService, UserMapper userMapper) {
+        this.eventService = eventService;
+        this.eventMapper = eventMapper;
+        this.commentService = commentService;
+        this.commentMapper = commentMapper;
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @PostMapping
+    public ResponseEntity<EventResponse> create(@RequestBody EventRequest eventRequest) {
+
+        Event event = eventMapper.toEntity(eventRequest);
+        Event savedEvent = eventService.save(event);
+        EventResponse eventResponse = eventMapper.toDto(savedEvent);
+
+        return new ResponseEntity<>(eventResponse, HttpStatus.CREATED);
+    }
 }
---
+++ src/test/java/com/vlaur/project/eventsapp/controller/EventRestControllerTest.java
@@ -0,0 +1,58 @@
+package com.vlaur.project.eventsapp.controller;
+
+import com.vlaur.project.eventsapp.RestIntegrationTest;
+import com.vlaur.project.eventsapp.dto.event.EventRequest;
+import com.vlaur.project.eventsapp.dto.event.EventResponse;
+import com.vlaur.project.eventsapp.model.Access;
+import com.vlaur.project.eventsapp.model.User;
+import com.vlaur.project.eventsapp.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventRestControllerTest extends RestIntegrationTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void givenEvent_whenCreateEventIsCalled_thenEventIsCreated() {
+        //given
+        User user = new User();
+        user.setFirstName("first name");
+        user.setLastName("last name");
+        User savedUser = userRepository.save(user);
+
+        EventRequest eventRequest = new EventRequest();
+        eventRequest.setName("my event");
+        eventRequest.setAddress("here");
+        eventRequest.setDate(LocalDate.now());
+        eventRequest.setAccess(Access.PUBLIC);
+        eventRequest.setOrganiserId(savedUser.getId());
+
+        //when
+        ResponseEntity<EventResponse> responseEntity =
+                testRestTemplate.postForEntity(EventRestController.API_EVENTS, eventRequest, EventResponse.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(responseEntity.getBody()).isNotNull();
+        assertThat(responseEntity.getBody().getName()).isEqualTo(eventRequest.getName());
+        assertThat(responseEntity.getBody().getAddress()).isEqualTo(eventRequest.getAddress());
+        assertThat(responseEntity.getBody().getAccess()).isEqualTo(eventRequest.getAccess());
+        assertThat(responseEntity.getBody().getOrganiser().getId()).isEqualTo(eventRequest.getOrganiserId());
+        assertThat(responseEntity.getBody().getOrganiser().getFirstName()).isEqualTo(user.getFirstName());
+        assertThat(responseEntity.getBody().getOrganiser().getLastName()).isEqualTo(user.getLastName());
+        assertThat(responseEntity.getBody().getDate()).isEqualTo(eventRequest.getDate());
+    }
+
+}
