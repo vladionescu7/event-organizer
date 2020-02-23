f2969a39da5626d585cf33e84483011d22e24334
laurmihai88 <panaete.laurentiu@gmail.com>
2020-02-23 09:41:21 +0200

    front end

--- frontend/event-organizer/src/app/features/user/user.service.ts
+++ frontend/event-organizer/src/app/features/user/user.service.ts
@@ -1,18 +1,30 @@
+import { OktaAuthService } from '@okta/okta-angular';
 import {IUser} from '../../shared/model/user';
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { apiUrl } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
-    
-    constructor(private httpClient: HttpClient){
+    private static USERS_API = "/api/users";
+    constructor(private httpClient: HttpClient,
+        private OktaAuthService: OktaAuthService ) {
 
     }
 
-saveUser(user: IUser){
+saveUser(user: IUser): Observable<IUser> {
+    return this.httpClient.post<IUser>(`${apiUrl}/${UserService.USERS_API}`, user)
 
+}
+
+getUserByOktaUserId(oktaUserId: string): Observable<IUser>{
+    const accessToken = this.OktaAuthService.getAccessToken().then(accessToken => )
+    const headers = new HttpHeaders( {
+        Authorization: 'Bearer ' + accessToken
+    });
 }
 
 }
